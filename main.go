e8213ac504030dd90ba7c8b2c88d45597682d160
chaewonkong <chaewonkong@gmail.com>
2025-04-22 23:45:21 +0900

    refactor: add run func

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"log/slog"
+	"os"
+
 	"github.com/chaewonkong/echo-demo/handler"
 	"github.com/chaewonkong/echo-demo/route"
 	"github.com/chaewonkong/echo-demo/router"
@@ -8,6 +12,20 @@ import (
 )
 
 func main() {
+	err := run()
+	if err != nil {
+		slog.Default().Error("failed to run server", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// load env
+	port := os.Getenv("PORT")
+	if port == "" {
+		return errors.New("port not set")
+	}
+
 	e := echo.New()
 	h := handler.Handler{}
 	r := router.NewRouter(e, h)
@@ -15,4 +33,6 @@ func main() {
 		route.UserRoutes,
 		route.PostRoutes,
 	)
+
+	return nil
 }
